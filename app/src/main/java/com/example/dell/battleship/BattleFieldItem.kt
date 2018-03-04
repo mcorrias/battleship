ef529fdfadf5ddd6185e75d8b0d043895ae0411f
mCorrias <corrias.michele@gmail.com>
2018-03-04 20:07:58 +0000

    game board game created

---
+++ app/src/main/java/com/example/dell/battleship/BattleFieldItem.kt
@@ -0,0 +1,6 @@
+package com.example.dell.battleship
+
+/**
+ * Created by Dell on 04/03/2018.
+ */
+class BattleFieldItem (var image : Int)
--- app/src/main/java/com/example/dell/battleship/GameActivity.kt
+++ app/src/main/java/com/example/dell/battleship/GameActivity.kt
@@ -2,11 +2,41 @@ package com.example.dell.battleship
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TableRow
+import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        createBattleField()
+    }
+
+
+
+    fun createBattleField(){
+
+        var rows  = 8
+        var colums = 8
+
+
+        for(i in 0..rows){
+            var row = TableRow(this)
+            for(j in 0..colums){
+                val item : ImageButton = ImageButton(this)
+                item.setBackgroundResource(R.drawable.item_drawable)
+                item.setOnClickListener(){
+                    item.setBackgroundResource(R.drawable.item_missed)
+                }
+                row.addView(item)
+            }
+            board.addView(row)
+        }
+
+
     }
 }
