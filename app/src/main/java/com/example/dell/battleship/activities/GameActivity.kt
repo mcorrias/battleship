7f50e7ce8cc94c7408557681722bb2d7f7221a7c
Michele Corrias <corrias.michele@gmail.com>
2020-12-19 19:02:17 +0100

    updated kotlin and gradle version and androidx added

--- app/src/main/java/com/example/dell/battleship/activities/GameActivity.kt
+++ app/src/main/java/com/example/dell/battleship/activities/GameActivity.kt
@@ -1,34 +1,35 @@
 package com.example.dell.battleship.activities
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TableRow
-import com.example.dell.battleship.engine.Game
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dell.battleship.R
+import com.example.dell.battleship.engine.Game
 import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
 
-    val newGame : Game = Game(this)
-    val rows  = 10
+    val newGame: Game = Game(this)
+    val rows = 10
     val colums = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        newGame.initGame(rows,colums)
+        newGame.initGame(rows, colums)
         createBattleField()
     }
 
-    private fun createBattleField(){
+    private fun createBattleField() {
 
-        for(i in 0..rows - 1 ){
+        for (i in 0..rows - 1) {
             var row = TableRow(this)
-            for(j in 0..colums - 1){
-                val item : ImageButton = ImageButton(this)
+            for (j in 0..colums - 1) {
+                val item: ImageButton = ImageButton(this)
                 item.setBackgroundResource(R.drawable.item_drawable)
-                item.setOnClickListener(){
+                item.setOnClickListener() {
                     attack(i, j, item)
                 }
                 row.addView(item)
@@ -37,8 +38,8 @@ class GameActivity : AppCompatActivity() {
         }
     }
 
-    private fun attack(x:Int , y:Int, item: ImageButton){
-        var resource = newGame.attack(x,y)
+    private fun attack(x: Int, y: Int, item: ImageButton) {
+        var resource = newGame.attack(x, y)
 
         item.setBackgroundResource(resource)
 
--- app/src/main/java/com/example/dell/battleship/activities/MainActivity.kt
+++ app/src/main/java/com/example/dell/battleship/activities/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.dell.battleship.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dell.battleship.R
 
 
